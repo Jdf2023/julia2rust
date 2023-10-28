bb1b601f93f61de207b5de0cabf3cd8c1b91d0f5
Jdf2023 <jophet.jdf.flores@gmail.com>
2023-10-28 15:33:08 -0700

    Create fib.rs

---
+++ examples/fib.rs
@@ -0,0 +1,16 @@
+fn main() {
+	let mut 	x = [0, 1, 2, 3, 4];
+	for i in x {
+		println!("{:?}", fib(i));
+	}
+}
+
+pub fn fib(n: i64) -> i64 {
+	if n == 0 {
+		return 0;
+	}	else if n == 1 {
+		return 1;
+	}	else {
+		return fib(n - 2) + fib(n - 1);
+	}
+}
