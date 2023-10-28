68ea77a0f4c8e2991c5e347a250d72d24e706fbb
Jdf2023 <jophet.jdf.flores@gmail.com>
2023-10-28 15:24:57 -0700

    Update examples

--- examples/fib.jl
+++ examples/fib.jl
@@ -1,3 +1,10 @@
+function main()
+	x = [0, 1, 2, 3, 4]
+	for i in x
+		println(fib(i))
+	end
+end
+
 function fib(n :: Int64) :: Int64
 	if n == 0
 		return 0
--- examples/main.jl
+++
@@ -1,8 +0,0 @@
-include("fib.jl")
-
-function main()
-	x = [0, 1, 2, 3, 4]
-	for i in x
-		println(fib(i))
-	end
-end
